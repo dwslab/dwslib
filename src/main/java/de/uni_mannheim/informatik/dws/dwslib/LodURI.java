6c67e1cd55f83c0b3ef8f62c812ce12245311a0b
mschuhma <michael@informatik.uni-mannheim.de>
2013-04-04 07:46:01 -0700

    LodURI changed to singelton class

--- src/main/java/de/uni_mannheim/informatik/dws/dwslib/LodURI.java
+++ src/main/java/de/uni_mannheim/informatik/dws/dwslib/LodURI.java
@@ -11,6 +11,8 @@ public class LodURI {
 	
 	final static Logger log = LoggerFactory.getLogger(LodURI.class);
 	
+	private static LodURI instance = null;
+	
 	public static File f = null;
 	public static HashMap<String, String> prefixMap = null;
 	public static HashMap<String, String> uriMap = null;
@@ -31,28 +33,38 @@ public class LodURI {
 		return uriMap.get(prefixedURI.substring(0, i))+prefixedURI.substring(i+1);	
 	}
 	
-	public LodURI(String uriList) {
+	private LodURI(String uriList) {
 		f = new File(uriList);
-		if (prefixMap == null && uriMap == null) {
-			prefixMap = new HashMap<String, String>();
-			uriMap = new HashMap<String, String>();
-			for (ArrayList<String> line : MyFileReader.readXSVFile(f, "\t", false)) {
-				prefixMap.put(line.get(1).trim(), line.get(0).trim() + ":");
-				uriMap.put(line.get(0).trim(), line.get(1).trim());
-			}
+		prefixMap = new HashMap<String, String>();
+		uriMap = new HashMap<String, String>();
+		for (ArrayList<String> line : MyFileReader.readXSVFile(f, "\t", false)) {
+			prefixMap.put(line.get(1).trim(), line.get(0).trim() + ":");
+			uriMap.put(line.get(0).trim(), line.get(1).trim());
 		}
-		log.debug("PrefixCC Mappings loaded from file {}", f.getName());
+		log.debug("PrefixCC mappings loaded from file {}", f.getName());
 	}
 	
-	public LodURI() {
-		this("src/main/java/de/uni_mannheim/informatik/dws/dwslib/prefixcc-20130404.tab");
+	
+	public static LodURI getInstance(String uriList) {
+		if (instance == null)
+			instance = new LodURI(uriList);
+		return instance;
+	}
+	
+	public static LodURI getInstance() {
+		if (instance == null)
+			instance = new LodURI("src/main/resources/prefixcc-20130404.tab");
+		return instance;
 	}
 
-
 	public static void main(String[] args) {
-		LodURI uri = new LodURI();
-		System.out.println(uri.toFullUri("rdf:type"));
-		System.out.println(uri.toPrefixedUri("http://dbpedia.org/class/yago/Operas"));
+		LodURI uri = LodURI.getInstance();
+		
+		System.out.println("toFullUri(\"rdf:type\") = "+uri.toFullUri("rdf:type"));
+		
+		System.out.println(
+				"toPrefixedUri(\"http://dbpedia.org/class/yago/Operas\") = " +
+				uri.toPrefixedUri("http://dbpedia.org/class/yago/Operas"));
 	}
 
 }
