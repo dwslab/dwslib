8c7de3b410445b80615231fe9374d6772a2597f4
Robert <robert.meusel@freenet.de>
2013-07-22 05:10:27 -0700

    added 2nd output type for writer (plain & gzip)

--- src/main/java/de/uni_mannheim/informatik/dws/dwslib/util/BufferedChunkingWriter.java
+++ src/main/java/de/uni_mannheim/informatik/dws/dwslib/util/BufferedChunkingWriter.java
@@ -4,9 +4,12 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.zip.GZIPOutputStream;
+
+import de.uni_mannheim.informatik.dws.dwslib.util.model.OutputType;
 
 /**
  * This class wraps a {@link BufferedWriter} which makes use of an underlying
@@ -25,6 +28,7 @@ public class BufferedChunkingWriter {
 	private int nextChunk;
 	private File outputDir;
 	private File currentOutputFile;
+	private OutputType outputType = OutputType.GZIP;
 
 	/**
 	 * Initializes the writer. Please not that at this point no file or writer
@@ -38,9 +42,11 @@ public class BufferedChunkingWriter {
 	 * @param maxFileSize
 	 *            the maximal size of the output file in MB. Will internally by
 	 *            converted to Bytes (*1024*1024).
+	 * @param type
+	 *            the Type of the output files.
 	 */
 	public BufferedChunkingWriter(File outputDir, String nameScheme,
-			int maxFileSize) {
+			int maxFileSize, OutputType type) {
 		this.maxFileSize = (long) maxFileSize * 1024 * 1024;
 		this.nameScheme = nameScheme.trim();
 		if (!outputDir.exists()) {
@@ -53,6 +59,26 @@ public class BufferedChunkingWriter {
 		}
 		this.outputDir = outputDir;
 		this.nextChunk = 0;
+		this.outputType = type;
+	}
+
+	/**
+	 * Initializes the writer (default GZIP). Please not that at this point no file or writer
+	 * is initialized internally. This will be done at the first time
+	 * {@link #write(String)} is called.
+	 * 
+	 * @param outputDir
+	 *            the directory where to write the output files.
+	 * @param nameScheme
+	 *            the prefix-namescheme ot be used.
+	 * @param maxFileSize
+	 *            the maximal size of the output file in MB. Will internally by
+	 *            converted to Bytes (*1024*1024).
+	 */
+	public BufferedChunkingWriter(File outputDir, String nameScheme,
+			int maxFileSize) {
+		new BufferedChunkingWriter(outputDir, nameScheme, maxFileSize,
+				OutputType.GZIP);
 	}
 
 	/**
@@ -92,9 +118,15 @@ public class BufferedChunkingWriter {
 				e.printStackTrace();
 			}
 		}
+		if (outputType == OutputType.GZIP){
 		currentOutputFile = new File(outputDir, nameScheme + "-"
 				+ String.format("%05d", nextChunk++) + ".gz");
 		currentWriter = new BufferedWriter(new OutputStreamWriter(
 				new GZIPOutputStream(new FileOutputStream(currentOutputFile))));
+		}else if(outputType == OutputType.PLAIN){
+			currentOutputFile = new File(outputDir, nameScheme + "-"
+					+ String.format("%05d", nextChunk++));
+			currentWriter = new BufferedWriter(new FileWriter(currentOutputFile));
+		}
 	}
 }
---
+++ src/main/java/de/uni_mannheim/informatik/dws/dwslib/util/model/OutputType.java
@@ -0,0 +1,8 @@
+package de.uni_mannheim.informatik.dws.dwslib.util.model;
+
+public enum OutputType {
+	
+	PLAIN,
+	GZIP;
+
+}
