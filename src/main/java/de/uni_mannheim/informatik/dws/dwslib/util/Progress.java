4dd6487a172681927af61ffa7562205edd24d222
Robert <robert.meusel@freenet.de>
2013-11-07 05:18:20 -0800

    added progress

---
+++ src/main/java/de/uni_mannheim/informatik/dws/dwslib/util/Progress.java
@@ -0,0 +1,26 @@
+package de.uni_mannheim.informatik.dws.dwslib.util;
+/**
+ * Progress counter
+ * @author Oliver Lehmberg
+ *
+ */
+
+public class Progress {
+	public long count;
+	public long interval;
+	public String message;
+	
+	public Progress(int interval, String message)
+	{
+		count = 0;
+		this.interval = interval;
+		this.message = message;
+	}
+	
+	public void IncrementAndPrint()
+	{
+		count++;
+		if(count % interval == 0)
+			System.out.println(count + " " + message);
+	}
+}
