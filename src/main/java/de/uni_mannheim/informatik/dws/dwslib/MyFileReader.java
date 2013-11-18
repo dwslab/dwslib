89a766b79b4f0cfba63624e32d5b7e1fc9861d37
mschuhma <michael@informatik.uni-mannheim.de>
2013-11-18 14:40:28 +0100

    BugFix MyFileReader -> Now reading files as resources from within jar file (cherry picked from commit 29382c3)

--- src/main/java/de/uni_mannheim/informatik/dws/dwslib/MyFileReader.java
+++ src/main/java/de/uni_mannheim/informatik/dws/dwslib/MyFileReader.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -19,43 +20,96 @@ public class MyFileReader {
 	
 	final static Logger log = LoggerFactory.getLogger(MyFileReader.class);
 	
-	public static ArrayList<ArrayList<String>> readXSVFile(File inputFile, String valueSeperator, boolean hasHeader) {
+	public static ArrayList<ArrayList<String>> readXSVFile(
+				InputStream inputStream, String valueSeperator, boolean hasHeader) {
+		
 		Scanner scan = null;
-		try {
-			scan = new Scanner(inputFile, "UTF-8");
-			if (hasHeader) {scan.nextLine();}
-		} catch (FileNotFoundException e) {
-			log.warn(e.toString());
-		}
+		scan = new Scanner(inputStream, "UTF-8");
+		
+		if (hasHeader) {scan.nextLine();}
 		ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
+		
 		while (scan.hasNextLine()) {
+			
 			ArrayList<String> tokens = new ArrayList<String>();
+			
 			String line = scan.nextLine();
 			StringTokenizer st = new StringTokenizer(line,valueSeperator);
+			
 			while (st.hasMoreTokens()) {
 				tokens.add(st.nextToken());
 				}
 			lines.add(tokens);
 			}
+		
 		scan.close();
+		
 		return lines;
 	}
 	
-	public static ArrayList<String> readLinesFile(File inputFile) {
+	public static ArrayList<ArrayList<String>> readXSVFile(
+				File inputFile, String valueSeperator, boolean hasHeader) {
+		
+		try {
+	      return readXSVFile(
+	      			new FileInputStream(inputFile), 
+	      			valueSeperator, hasHeader);
+      }
+      catch (FileNotFoundException e) {
+	      log.error(e.toString());
+	      return null;
+      }
+	}
+	
+	public static ArrayList<ArrayList<String>> readXSVFile(
+				String inputFile, String valueSeperator, boolean hasHeader) {
+		
+		return readXSVFile(
+					MyFileReader.class.getResourceAsStream(inputFile), 
+					valueSeperator, hasHeader);
+		
+	}
+	
+	
+	public static ArrayList<String> readLinesFile(InputStream inputStream) {
 		
 		BufferedReader br = null;
 		ArrayList<String> lines = new ArrayList<String>();
 		
 		try {
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), Charset.forName("UTF-8")));
+			br = new BufferedReader(new InputStreamReader(
+						inputStream, Charset.forName("UTF-8")));
+			
 			while (br.ready()){
 				lines.add(br.readLine());
 			}
 			br.close();
+			
 		} catch (IOException e) {
 			log.error(e.toString());
 		}
 		return lines;
+	}
+	
+	public static ArrayList<String> readLinesFile(File inputFile) {
+		
+		try {
+			return readLinesFile(
+	      			new FileInputStream(inputFile));
+      }
+      catch (FileNotFoundException e) {
+			log.error(e.toString());
+			return null;
+      }
+		
+	}
+	
+	
+	public static ArrayList<String> readLinesFile(String inputFile) {
+		
+		return readLinesFile(
+					MyFileReader.class.getResourceAsStream(inputFile));
+		
 	}
 
 }
