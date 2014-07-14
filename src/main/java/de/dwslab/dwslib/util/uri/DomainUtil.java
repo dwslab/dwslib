41a83b7b1f0922c0ac7aab328eb41d51096c0936
Robert <robert.meusel@freenet.de>
2014-07-14 16:05:59 +0200

    reorganized package structure removed deprecated classes (see https://github.com/dwslab/dwslib/wiki/Meeting-Minutes-2014-07-09)

--- src/main/java/de/dwslab/dwslib/util/uri/DomainUtil.java
+++ src/main/java/de/dwslab/dwslib/util/uri/DomainUtil.java
@@ -136,7 +136,7 @@ public class DomainUtil {
 		String domain = getDomain(url);
 		try {
 			InternetDomainName fullDomainName = InternetDomainName.from(domain);
-			return fullDomainName.topPrivateDomain().name();
+			return fullDomainName.topPrivateDomain();
 		} catch (Exception e) {
 			//log.log(Level.WARNING, "Could not get pld from " + domain + ".");
 		}
