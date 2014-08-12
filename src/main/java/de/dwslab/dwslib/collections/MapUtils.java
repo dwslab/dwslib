8a68fa9b75ba2008a49c8a28b787182f53fd94d8
mschuhma <michael@informatik.uni-mannheim.de>
2014-08-12 09:59:47 +0200

    MapUtils cleaned

--- src/main/java/de/dwslab/dwslib/collections/MapUtils.java
+++ src/main/java/de/dwslab/dwslib/collections/MapUtils.java
@@ -10,26 +10,42 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 
+ * Frequently used static Utils methods for {@link Map}
  * @author Michael Schuhmacher
  *
  */
 public class MapUtils {
 	
 	/**
-	 * Sorts a map by values using comparator of the value object.
-	 * @param map
-	 * @return The sorted map as {@link LinkedHashMap}.
+	 * {@code SORTING_ORDER.ASCENDING} -> 1,2,3,4 <br>
+	 * {@code SORTING_ORDER.DESCENDING} -> 4,3,2,1
+	 */
+	enum SORTING_ORDER {ASCENDING, DESCENDING};
+	
+	
+	/**
+	 * Sorts a map by their values using the comparator of the value object.
+	 * @param map The map to be sorted
+	 * @param sortingOrder The {@link SORTING_ORDER}: <br> 
+	 * {@code SORTING_ORDER.ASCENDING} -> 1,2,3,4 <br>
+	 * {@code SORTING_ORDER.DESCENDING} -> 4,3,2,1
+	 * @return The sorted map as {@code LinkedHashMap}.
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static <K extends Object, V extends Comparable<V>> Map<K, V> sortByValue(Map<K, V> map) {
+	public static <K extends Object, V extends Comparable<V>> Map<K, V> sortByValue(
+			Map<K, V> map, final SORTING_ORDER sortingOrder) {
 		LinkedHashMap<K, V> orderedMap = new LinkedHashMap<K, V>();
 		List list = new LinkedList(map.entrySet());
 		
 		Collections.sort(list, new Comparator() {
 			public int compare(Object o1, Object o2) {
-				return ((Comparable) ((Map.Entry) (o2)).getValue())
-                        .compareTo(((Map.Entry) (o1)).getValue());
+				int val = ((Comparable) ((Map.Entry) (o1)).getValue()) .compareTo(((Map.Entry) (o2)).getValue());
+				if (sortingOrder == SORTING_ORDER.ASCENDING)
+					return val;
+				else if (sortingOrder == SORTING_ORDER.DESCENDING)
+					return -val;
+				else 
+					return 0;
 			}
 		});
 		
@@ -43,7 +59,7 @@ public class MapUtils {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Demonstrating usage of sortByValue() ...");
+		System.out.println("Demonstrating usage of sortByValueAsc() and sortByValueDsc() ...");
         
 		//creating Hashtable for sorting
         Map<String, Integer> olympic2012 = new HashMap<String, Integer>();
@@ -57,9 +73,13 @@ public class MapUtils {
         //printing hashtable without sorting
         System.out.println("Unsorted Map in Java : " + olympic2012);
      
-        //sorting Map e.g. HashMap, Hashtable by keys in Java
-        Map<String, Integer> sorted = sortByValue(olympic2012);
-        System.out.println("Sorted Map in Java by key: " + sorted);
+        //sorting Map by keys
+        Map<String, Integer> sortedAsc = sortByValue(olympic2012, MapUtils.SORTING_ORDER.ASCENDING);
+        System.out.println("Ascending sorted Map in Java by key: " + sortedAsc);
+        
+        //sorting Map by keys
+        Map<String, Integer> sortedDsc = sortByValue(olympic2012, MapUtils.SORTING_ORDER.DESCENDING);
+        System.out.println("Descending sorted Map in Java by key: " + sortedDsc);
 
 	}
 
