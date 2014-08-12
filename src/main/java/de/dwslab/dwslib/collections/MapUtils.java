b709349b5280ff8d4027235d5339fa89864a7bae
mschuhma <michael@informatik.uni-mannheim.de>
2014-08-12 10:07:04 +0200

    Added enum SortingOrderTypes

--- src/main/java/de/dwslab/dwslib/collections/MapUtils.java
+++ src/main/java/de/dwslab/dwslib/collections/MapUtils.java
@@ -9,40 +9,35 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import de.dwslab.dwslib.models.SortingOrderTypes;
+
 /**
  * Frequently used static Utils methods for {@link Map}
  * @author Michael Schuhmacher
  *
  */
-public class MapUtils {
-	
-	/**
-	 * {@code SORTING_ORDER.ASCENDING} -> 1,2,3,4 <br>
-	 * {@code SORTING_ORDER.DESCENDING} -> 4,3,2,1
-	 */
-	enum SORTING_ORDER {ASCENDING, DESCENDING};
-	
+public class MapUtils {	
 	
 	/**
 	 * Sorts a map by their values using the comparator of the value object.
 	 * @param map The map to be sorted
-	 * @param sortingOrder The {@link SORTING_ORDER}: <br> 
-	 * {@code SORTING_ORDER.ASCENDING} -> 1,2,3,4 <br>
-	 * {@code SORTING_ORDER.DESCENDING} -> 4,3,2,1
+	 * @param sortingOrder The {@link SortingOrderTypes}: <br> 
+	 * {@code SortingOrderTypes.ASCENDING} -> 1,2,3,4 <br>
+	 * {@code SortingOrderTypes.DESCENDING} -> 4,3,2,1
 	 * @return The sorted map as {@code LinkedHashMap}.
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static <K extends Object, V extends Comparable<V>> Map<K, V> sortByValue(
-			Map<K, V> map, final SORTING_ORDER sortingOrder) {
+			Map<K, V> map, final SortingOrderTypes sortingOrder) {
 		LinkedHashMap<K, V> orderedMap = new LinkedHashMap<K, V>();
 		List list = new LinkedList(map.entrySet());
 		
 		Collections.sort(list, new Comparator() {
 			public int compare(Object o1, Object o2) {
 				int val = ((Comparable) ((Map.Entry) (o1)).getValue()) .compareTo(((Map.Entry) (o2)).getValue());
-				if (sortingOrder == SORTING_ORDER.ASCENDING)
+				if (sortingOrder == SortingOrderTypes.ASCENDING)
 					return val;
-				else if (sortingOrder == SORTING_ORDER.DESCENDING)
+				else if (sortingOrder == SortingOrderTypes.DESCENDING)
 					return -val;
 				else 
 					return 0;
@@ -74,11 +69,11 @@ public class MapUtils {
         System.out.println("Unsorted Map in Java : " + olympic2012);
      
         //sorting Map by keys
-        Map<String, Integer> sortedAsc = sortByValue(olympic2012, MapUtils.SORTING_ORDER.ASCENDING);
+        Map<String, Integer> sortedAsc = sortByValue(olympic2012, SortingOrderTypes.ASCENDING);
         System.out.println("Ascending sorted Map in Java by key: " + sortedAsc);
         
         //sorting Map by keys
-        Map<String, Integer> sortedDsc = sortByValue(olympic2012, MapUtils.SORTING_ORDER.DESCENDING);
+        Map<String, Integer> sortedDsc = sortByValue(olympic2012, SortingOrderTypes.DESCENDING);
         System.out.println("Descending sorted Map in Java by key: " + sortedDsc);
 
 	}
---
+++ src/main/java/de/dwslab/dwslib/models/SortingOrderTypes.java
@@ -0,0 +1,13 @@
+package de.dwslab.dwslib.models;
+
+/**
+ * {@code SORTING_ORDER.ASCENDING} -> 1,2,3,4 <br>
+ * {@code SORTING_ORDER.DESCENDING} -> 4,3,2,1
+ */
+
+public enum SortingOrderTypes {
+	
+	ASCENDING, 
+	DESCENDING;
+	
+}
